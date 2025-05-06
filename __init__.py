c6069f9678d1fcbb808b44c9040e1a224d9b6582
Rohit Singhee <rohitsinghee@gmail.com>
2025-05-06 12:14:31 +0530

    Updtae schema manager

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+Central schema definitions for IdeaVerse services
+
+Import schemas directly from their modules:
+from schema_manager.idea import IdeaGenerationTask, IdeaResponse, etc.
+from schema_manager.user import ...
+"""
+
+# No imports here - users will import directly from modules 
